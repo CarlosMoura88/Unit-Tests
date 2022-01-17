2041197469fbd8c3c53d0f86694960105637334f
Carlos Moura <carlosmourapires88@gmail.com>
2022-01-17 16:00:38 -0300

    Requisito 1 concluido

--- src/average.js
+++ src/average.js
@@ -12,6 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {} ;
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) return undefined;
+  for (let valor of array) {    
+    if (typeof valor !== 'number') return undefined;
+    sum += valor;
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
